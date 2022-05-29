4529c8184937892a143205c9cb6d42b32c758de8
iigunchev <iigunchev@gmail.com>
2022-05-29 15:10:35 +0200

    feat: auth modal, layout and input

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,20 @@ import styles from '../styles/Home.module.css';
 
 import data from '../data/data.json';
 import Grid from '../components/Grid';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Grid items={data}/>
-    </div>
+    <Layout>
+      <h1 className="text-xl font-medium text-gray-800">
+        Top-rated
+      </h1>
+      <p className="text-gray-500">
+        Explore the funniest Gifs and Memes
+      </p>
+      <div className="mt-8">
+        <Grid items={data} />
+      </div>
+    </Layout>
   );
 }
